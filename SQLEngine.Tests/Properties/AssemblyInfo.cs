abfae69534c94fbcaed45969b2eccba8780ae7ed
raminrahimzada@gmail.com <raminrahimzada@gmail.com>
2020-07-28 23:54:34 +0400

    Converted to .NET standard

--- SQLEngine.Tests/Properties/AssemblyInfo.cs
+++ SQLEngine.Tests/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SQLEngine.Tests")]
@@ -6,13 +7,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SQLEngine.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("8069c256-4569-4a6a-bc67-e3fd3b980b07")]
+[assembly: Guid("2932f954-4003-4af7-b67d-5dddcc9d13e0")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
